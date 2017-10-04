dcd267e8b89976d9055ad67eea6d2cf38bb435be
cristian27dam <cristian.margar23@gmail.com>
2017-10-04 10:40:25 +0200

    Añadimos la localidad de nacimiento

--- Desktop/Github/ejemplo2.java
+++ Desktop/Github/ejemplo2.java
@@ -2,6 +2,8 @@ public class ejemplo{
   public static void main(String args[]) {
 	System.out.println("Hola mundo");
 	System.out.println("Añadimos la Edad");
+	System.out.println("Añadimos la localidad de nacimiento");
+
        
 }
   
